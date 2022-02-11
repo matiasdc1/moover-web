faf72086ef76b7a6436cddce0b5d6cf4be6458fc
sensecryptos <matiasdiezcanseco@sensecryptos.com>
2022-02-11 16:14:40 -0500

    Added Custom 404 Page.
    Added Custom 404 Page and the component 404 inside the components/general folder.

---
+++ components/general/404.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="flex flex-col md:flex-row items-center justify-center text-3xl h-full">
+    <h1 class="font-semibold md:mr-2 md:border-r-2 md:pr-2">404</h1>
+    <h2 class="text-lg md:text-2xl">Página no encontrada.</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: '404',
+}
+</script>
+
+<style></style>
